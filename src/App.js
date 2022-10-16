ed4167060bb3e65e7b931f7b0473f85e13d39b03
xpressiscool <nhbabb@gmail.com>
2022-10-16 11:01:55 -0500

    add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const day = date.getDate();
   const month = date.getMonth();
   const year = date.getFullYear()
-  const header = month + day + year
+  const header = month + "/" + day + "/"+ year
   let text;
   if (day === 25 && month === 12) {
     text = "Yes"
@@ -17,6 +17,7 @@ function App() {
       <header className="App-header">
         <h1 className='text'>
           Is today christmas: <span class="christmas">{text}</span>
+          <br/>
           {header}
         </h1>
       </header>
